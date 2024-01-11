951b4d7793e31e3c71278805a715bc33fa94d01a
Artem Karelin <temi41995@gmail.com>
2024-01-11 16:44:01 +0100

    feat: create project structure and base components

---
+++ src/components/app-info/app-info.tsx
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employee accounting</h1>
+            <h2>Employees: </h2>
+            <h2>Bonuses: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
